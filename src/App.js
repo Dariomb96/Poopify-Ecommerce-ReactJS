4db50f4bb462ee1b37e2db3f71565424ab2034bf
Dariomb96 <betancourtdariom@gmail.com>
2022-08-17 15:20:17 -0300

    nueva app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './tapa16.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testeito de reactsito
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://superamaia.files.wordpress.com/2010/09/fv.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          toma una galletita
         </a>
       </header>
     </div>
